efd63dc19d56d9b35ab63a5daf5156da9fbe7223
babs aina <ainababs0@gmail.com>
2023-04-02 15:34:13 -0400

    tested registration schema connection to nexas database

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const express = require("express");
-const mongoose = require("mongoose");
+import express from "express";
+import mongoose from "mongoose";
+import User from "./model/User.js"; 
+
 const app = express();
 
 const uri = "mongodb+srv://rootuser:password%401@nexascluster.8876mb9.mongodb.net/?retryWrites=true&w=majority";
@@ -8,7 +10,7 @@ async function connect() {
   try {
     await mongoose.connect(uri);
     console.log("Connected to MongoDB");
-  
+
   } catch (error) {
     console.error(error);
   }
@@ -19,5 +21,17 @@ connect();
 
 app.listen(8000, () => {
   console.log("server started on port 8000");
-
 });
+
+async function run() {
+  const user = new User({
+    username: "babs",
+    password: "Swexxx",
+    email: "ainababs0@gmail",
+    gender: "male"
+  });
+  await user.save();
+  console.log(user);
+}
+
+run();
