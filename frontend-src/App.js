9ec8cd4d1abd99093dcc7fec08d50efc59845bd3
Evan Phaup <56270325+EvanPhaup@users.noreply.github.com>
2023-04-21 23:17:10 -0400

    Settings and Profile Pages Added

--- frontend-src/App.js
+++ frontend-src/App.js
@@ -2,11 +2,14 @@ import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import Home from './views/Home';
 import Login from "./views/Login";
+import Settings from "./views/Settings"
+import Profile from "./views/Profile"
 import { Route, Routes } from 'react-router-dom';
 import MatchPeers from './views/MatchPeers';
+/*
 import dotenv from 'dotenv';
 dotenv.config();
-
+*/
 
 function App() {
 
@@ -19,6 +22,8 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="login" element={<Login />}></Route>
         <Route path="match-peers" element={<MatchPeers />}></Route>
+        <Route path="settings" element={<Settings />}></Route>
+        <Route path="profile" element={<Profile />}></Route>
       </Routes>
     </div>
   </>
--- frontend-src/index.js
+++ frontend-src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+/*
 import dotenv from 'dotenv';
 dotenv.config();
-
+*/
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
