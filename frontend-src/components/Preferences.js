7c915945e2d1c85c8f38b7e8f0e945635a709fc6
Evan Phaup <56270325+EvanPhaup@users.noreply.github.com>
2023-04-26 21:53:27 -0400

    Update Preferences.js

--- frontend-src/components/Preferences.js
+++ frontend-src/components/Preferences.js
@@ -1,23 +1,201 @@
+import { useState } from "react";
+
 const Preferences = () => {
     return (
         <div className="preferences">
             <p>Preferences</p>
-            <div className="preferences-line1">
-                <div className="proficiencies">
-                    <p>Proficiences</p>
+            <div className="proficiencies-label">
+                <p>Proficiences</p>
+            </div>
+            <div className="proficiencies-languages">
+                <div className="language-header">
+                    <p>Languages:</p>
                 </div>
-            </div>
-            <div className="preferences-line2">
-                <div className="interests">
-                    <p>Interests</p>
+                <div className="language-options">
+                    <label>
+                        <input 
+                            type="checkbox"
+                        />
+                        {" "} Java
+                    </label>
+                    <label>
+                        <input 
+                            type="checkbox"
+                        />
+                        {" "} C
+                    </label>
+                    <label>
+                        <input 
+                            type="checkbox"
+                        />
+                        {" "} Python
+                    </label>
+                    <label>
+                        <input 
+                            type="checkbox"
+                        />
+                        {" "} JavaScript
+                    </label>
                 </div>
+            </div>    
+            <div className="proficiencies-platforms">
+                <div className="platform-header">
+                    <p>Platforms:</p>
+                </div>
+                <div className="platform-options">
+                    <label>
+                        <input 
+                            type="checkbox"
+                        />
+                        {" "} CodeChef
+                    </label>
+                    <label>
+                        <input 
+                            type="checkbox"
+                        />
+                        {" "} LeetCode
+                    </label>
+                    <label>
+                        <input 
+                            type="checkbox"
+                        />
+                        {" "} HackerRank
+                    </label>
+                    <label>
+                        <input 
+                            type="checkbox"
+                        />
+                        {" "} InterviewBit
+                    </label>
+                </div>
+            </div>    
+            <div className="proficiencies-domains">
+                <div className="domain-header">
+                    <p>Domains:</p>
+                </div>
+                <div className="domain-options">
+                    <label>
+                        <input 
+                            type="checkbox"
+                        />
+                        {" "} Front-End
+                    </label>
+                    <label>
+                        <input 
+                            type="checkbox"
+                        />
+                        {" "} Testing
+                    </label>
+                    <label>
+                        <input 
+                            type="checkbox"
+                        />
+                        {" "} Database Systems
+                    </label>
+                    <label>
+                        <input 
+                            type="checkbox"
+                        />
+                        {" "} Back-End
+                    </label>
+                </div>
+            </div>    
+            <div className="interests-label">
+                <p>Interests</p>
             </div>
-            <div className="preferences-line3">
-            <div className="match-radius">
-                <p>Match Radius</p>
-                <input />
-            </div>
-            </div>
+            <div className="interests-languages">
+                <div className="language-header">
+                    <p>Languages:</p>
+                </div>
+                <div className="language-options">
+                    <label>
+                        <input 
+                            type="checkbox"
+                        />
+                        {" "} Java
+                    </label>
+                    <label>
+                        <input 
+                            type="checkbox"
+                        />
+                        {" "} C
+                    </label>
+                    <label>
+                        <input 
+                            type="checkbox"
+                        />
+                        {" "} Python
+                    </label>
+                    <label>
+                        <input 
+                            type="checkbox"
+                        />
+                        {" "} JavaScript
+                    </label>
+                </div>
+            </div>    
+            <div className="interests-platforms">
+                <div className="platform-header">
+                    <p>Platforms:</p>
+                </div>
+                <div className="platform-options">
+                    <label>
+                        <input 
+                            type="checkbox"
+                        />
+                        {" "} CodeChef
+                    </label>
+                    <label>
+                        <input 
+                            type="checkbox"
+                        />
+                        {" "} LeetCode
+                    </label>
+                    <label>
+                        <input 
+                            type="checkbox"
+                        />
+                        {" "} HackerRank
+                    </label>
+                    <label>
+                        <input 
+                            type="checkbox"
+                        />
+                        {" "} InterviewBit
+                    </label>
+                </div>
+            </div>    
+            <div className="interests-domains">
+                <div className="domain-header">
+                    <p>Domains:</p>
+                </div>
+                <div className="domain-options">
+                    <label>
+                        <input 
+                            type="checkbox"
+                        />
+                        {" "} Front-End
+                    </label>
+                    <label>
+                        <input 
+                            type="checkbox"
+                        />
+                        {" "} Testing
+                    </label>
+                    <label>
+                        <input 
+                            type="checkbox"
+                        />
+                        {" "} Database Systems
+                    </label>
+                    <label>
+                        <input 
+                            type="checkbox"
+                        />
+                        {" "} Back-End
+                    </label>
+                </div>
+            </div>    
             <button className="save-button">
                 Save
             </button>
