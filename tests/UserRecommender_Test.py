448f8654781715285baf360559da72e7bfa34698
sadathkhn4 <87616089+sadathkhn4@users.noreply.github.com>
2023-05-06 23:43:17 -0400

    Update UserRecommender_Test.py

--- tests/UserRecommender_Test.py
+++ tests/UserRecommender_Test.py
@@ -1,21 +1,21 @@
 import unittest
 # import the is_prime function
-import algo
+import algo_
 #from algo import UserRecommender
 
 
 class TestMatches(unittest.TestCase):
     def test_155(self):
-        self.assertTrue(len(algo.UserRecommender(155)) == 11)
+        self.assertTrue(len(algo_.UserRecommender(155)) == 11)
 
     def test_166(self):
-        self.assertTrue(len(algo.UserRecommender(166)) == 11)
+        self.assertTrue(len(algo_.UserRecommender(166)) == 11)
 
     def test_142(self):
-        self.assertTrue(len(algo.UserRecommender(142)) == 11)
+        self.assertTrue(len(algo_.UserRecommender(142)) == 11)
 
     def test_189(self):
-        self.assertTrue(len(algo.UserRecommender(189)) == 11)
+        self.assertTrue(len(algo_.UserRecommender(189)) == 11)
 
 
 if __name__ == '__main__':
